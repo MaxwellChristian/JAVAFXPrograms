bb7965c9d79110e5aa7eb6e98dda27807b9e525a
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-03-10 08:35:44 -0600

    Maxwell Christian

--- src/main/java/com/maxwell/javafxprograms/event_handling/LoanExample.java
+++ src/main/java/com/maxwell/javafxprograms/event_handling/LoanExample.java
@@ -49,8 +49,7 @@ public class LoanExample extends Application {
         tfTotalPayment.setEditable(false);
         GridPane.setHalignment(btCalculate, HPos.RIGHT);
 
-        // Process events
-        btCalculate.setOnAction(e -> calculateLoanPayment());
+
 
         // Create a scene and place it in the stage
         Scene scene = new Scene(gridPane, 400, 250);
@@ -60,24 +59,5 @@ public class LoanExample extends Application {
 
     }
 
-    private void calculateLoanPayment() {
-
-        // Get values from text fields
-        double interest =
-                Double.parseDouble(tfAnnualInterestRate.getText());
-        int year = Integer.parseInt(tfNumberOfYears.getText());
-        double loanAmount =
-                Double.parseDouble(tfLoanAmount.getText());
-
-        // Create a loan object. Loan defined in Listing 10.2
-        Loan loan = new Loan(interest, year, loanAmount);
-
-        // Display monthly payment and total payment
-        tfMonthlyPayment.setText(String.format("$%.2f",
-                loan.getMonthlyPayment()));
-        tfTotalPayment.setText(String.format("$%.2f",
-                loan.getTotalPayment()));
-
-    }
 
 }
