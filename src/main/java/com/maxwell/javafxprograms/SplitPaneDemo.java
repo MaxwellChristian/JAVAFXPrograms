f1d7245aac35a1059477f4c70f084deffca505fa
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2023-03-16 12:02:07 -0600

    Maxwell Christian

---
+++ src/main/java/com/maxwell/javafxprograms/SplitPaneDemo.java
@@ -0,0 +1,63 @@
+package com.maxwell.javafxprograms;
+
+import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class SplitPaneDemo extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+
+        VBox vBox = new VBox(10);
+
+        // Radio Button with country names
+        RadioButton rbUS = new RadioButton("US");
+        RadioButton rbCA = new RadioButton("CA");
+        RadioButton rbUK = new RadioButton("UK");
+
+        vBox.getChildren().add(rbUS);
+        vBox.getChildren().add(rbCA);
+        vBox.getChildren().add(rbUK);
+
+        // Split pane
+        SplitPane splitPaneForButtons = new SplitPane();
+        splitPaneForButtons.setOrientation(Orientation.VERTICAL);
+
+        // Buttons for country description
+
+        StackPane spUS = new StackPane();
+        spUS.getChildren().add(new Button("US Description"));
+
+        StackPane spCA = new StackPane();
+        spCA.getChildren().add(new Button("CA Description"));
+
+        StackPane spUK = new StackPane();
+        spUK.getChildren().add(new Button("UK Description"));
+
+        splitPaneForButtons.getItems().add(spUS);
+        splitPaneForButtons.getItems().add(spCA);
+        splitPaneForButtons.getItems().add(spUK);
+
+        splitPaneForButtons.getItems().add(new ScrollPane(new TextArea("Welcome to description of companies")));
+
+        SplitPane splitPane = new SplitPane();
+        splitPane.getItems().add(vBox);
+        splitPane.getItems().add(splitPaneForButtons);
+
+        // scene
+        Scene scene = new Scene(splitPane, 300, 250);
+
+        stage.setScene(scene);
+        stage.setTitle("Split pane demo");
+
+        stage.show();
+
+
+
+    }
+}
